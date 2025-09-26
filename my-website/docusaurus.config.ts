13a8cd701ce1f052e6f265da91d71c05cc2a241b
anuragpujari <anurag.pujari123@gmail.com>
2025-09-26 19:52:09 +0530

    Changes

--- my-website/docusaurus.config.ts
+++ my-website/docusaurus.config.ts
@@ -7,7 +7,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 const config: Config = {
   title: 'Fluidzy Docs',
   tagline: 'Clarity starts here',
-  favicon: 'img/favicon.ico',
+  //favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
